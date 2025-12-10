e01b8e83f9638c12665fe1fa88c4b30fc1b8c35b
Gravifer <44160838+Gravifer@users.noreply.github.com>
2025-12-11 01:27:03 +0800

    feat(syscall): complete lab02

--- user/attack.c
+++ user/attack.c
@@ -3,10 +3,22 @@
 #include "user/user.h"
 #include "kernel/riscv.h"
 
+#define DATASIZE (10*4096)
+char buf[DATASIZE];
+
 int
 main(int argc, char *argv[])
 {
   // Your code here.
+  // explicitly use uninitialized memory
+  // for(int i = 9*4096; i < DATASIZE; i++) {
+  //   // inspect each byte
+  //   if(buf[i] != 0) {
+  //     printf("%d -> 0x%x (%c)\n", i, buf[i], buf[i]);
+  //   }
+  // }
+  // printf("Secret data leak: %s\n", &buf[1]);
+  printf("%s\n", &buf[36887]);
 
-  exit(1);
+  exit(0);
 }
